5a49e65de9750c88a2812759eb54ffb891509970
Jhamil <jhamilrodrigo26@gmail.com>
2024-03-07 20:54:49 -0500

    test

--- reserva.js
+++ reserva.js
@@ -1,17 +1,22 @@
 document.getElementById('reservaBoton').addEventListener('click', function() {
     
-        var width = 550;
-        var height = 250;
-        var left = (screen.width / 2) - (width / 2);
-        var top = (screen.height / 2) - (height / 2);
+    if (popupWindow && !popupWindow.closed) {
+        popupWindow.close();
+    }
 
+    var width = 550;
+    var height = 250;
+    var left = (screen.width / 2) - (width / 2);
+    var top = (screen.height / 2) - (height / 2);
+
+    var popupWindow = window.open('../reserva/reservaExitosa.html', '_blank', 'width=' + width + ',height=' + height + ',left=' + left + ',top=' + top);
+
+    // Esperar a que la ventana emergente esté completamente cargada antes de redireccionar
+     popupWindow = window.open('../reserva/reservaExitosa.html', '_blank', 'width=' + width + ',height=' + height + ',left=' + left + ',top=' + top);
         
-        popupWindow = window.open('../reserva/reservaExitosa.html', '_blank', 'width=' + width + ',height=' + height + ',left=' + left + ',top=' + top);
-
-        console.log("jhamil1")
-        setTimeout(function() {
-            window.location.href = '../index.html';
-        }, 10000)
-        console.log("jhamil2")
+        // Redirigir a index.html después de abrir la nueva ventana emergente
+    setTimeout(function() {
+        window.location.href = '../index.html';
+    }, 10000);
     
 });
